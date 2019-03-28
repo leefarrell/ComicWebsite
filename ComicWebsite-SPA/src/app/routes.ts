05ffab603e278af2fc77ef7ddc87d8e564dfd606
leefarrell <leefarrell45@hotmail.com>
2019-03-28 13:55:35 +0000

    tabs

---
+++ ComicWebsite-SPA/src/app/routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ComicListComponent } from './comic-list/comic-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListComponent } from './list/list.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            {path: 'comics', component: ComicListComponent},
+            {path: 'messages', component: MessagesComponent},
+            {path: 'list', component: ListComponent},
+        ]
+    },
+    {path: '**', redirectTo: '', pathMatch: 'full'},
+];
+
